de0d44274aea7e949c0807bcbd1e23d69ffd71e6
Yonatan Reicher <yony252525@gmail.com>
2021-04-08 20:37:10 +0300

    New main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 #[macro_use]
 mod jinjer;
 
+use std::io;
 use jinjer::*;
 use Inst::*;
 
 fn main() {
-    let mut vm = jinjer::VM::default();
-    emit!(vm, [
-        Nop, Nop,
-        get_const Value::int(2),
-        Nop, Nop, Nop,
-        get_const Value::int(3),
-        get_const Value::int(1),
-        Add,
-        Mul,
-    ]);
-    println!("Hello, world! {:?}", vm.run());
+    let s = "x + y";
+    let mut tokenizer = tokenizer::Tokenizer::from_reader(s.as_bytes()).unwrap();
+    let expr = parser::parse_expr(&mut tokenizer).unwrap();
+    println!("Expr: {:?}", expr);
+    // let mut vm = jinjer::VM::default();
+    // emit!(vm, [
+    //     Nop, Nop,
+    //     get_const Value::int(2),
+    //     Nop, Nop, Nop,
+    //     get_const Value::int(3),
+    //     get_const Value::int(1),
+    //     Add,
+    //     Mul,
+    // ]);
+    // println!("Hello, world! {:?}", vm.run());
 }
