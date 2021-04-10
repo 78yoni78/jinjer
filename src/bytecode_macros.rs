c694b6199a2792276341c7476d3f2d5565ad9887
Yonatan Reicher <yony252525@gmail.com>
2021-04-10 21:39:59 +0300

    Improved the macro

--- src/bytecode_macros.rs
+++ src/bytecode_macros.rs
@@ -1,13 +1,15 @@
 #[macro_export]
 macro_rules! emit {
-    ($self: ident) => {};
-    ($self: ident, []) => {};
-    ($self: ident, [get_const $value: expr $(, $($rest: tt)*)?]) => {
-        let x = $self.add_constant($value);
-        emit!($self, [crate::Inst::GetConst(x)]);
-        $(emit!($self, [$($rest)*]);)?
+    ($self: expr) => {};
+    ($self: expr, []) => {};
+    ($self: expr, [get_const $value: expr $(, $($rest: tt)*)?]) => {
+        {
+            let x = $self.add_constant($value);
+            emit!($self, [crate::Inst::GetConst(x)]);
+            $(emit!($self, [$($rest)*]);)?
+        }
     };
-    ($self: ident, [$inst: expr $(, $($rest: tt)*)?]) => {
+    ($self: expr, [$inst: expr $(, $($rest: tt)*)?]) => {
         $self.instructions.push($inst);
         $(emit!($self, [$($rest)*]);)?
     };
